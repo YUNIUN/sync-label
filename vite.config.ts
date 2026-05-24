15befee10ad4b39bf767c985b2fb082b391392db
YUNIUN <229393554@qq.com>
2026-05-24 15:47:25 +0800

    chore: 使用 vite 代替 rollup 作为编译和 demo 运行工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  root: './example',
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: '../dist/example',
+    outDir: resolve(__dirname, 'dist'),
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'SYNC_LABEL',
+      fileName: (format) => `index.${format}.js`
+    },
   },
+  plugins: [
+    dts({
+      tsconfigPath: resolve(__dirname, 'tsconfig.build.json'),
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
